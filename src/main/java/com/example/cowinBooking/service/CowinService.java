a6d68b5279ff0ef1a4c38d9190222e7e660af9a1
Sachin Sharma <sachin.sharma@walmart.com>
2021-05-21 17:11:14 +0530

    search by district

--- src/main/java/com/example/cowinBooking/service/CowinService.java
+++ src/main/java/com/example/cowinBooking/service/CowinService.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalUnit;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -31,13 +32,19 @@ public class CowinService {
     @Value("${pinCode}")
     String pinCode;
 
+    @Value("${cowin.district.code}")
+    String districtCode;
+
+    @Value("${cowin.search.district.base.url}")
+    String cowinBaseUrlSearchByDistrict;
+
     @Value("${age}")
     String ageLimit;
 
     @Value("${url}")
     String url;
 
-    @Scheduled(cron = "${frequency}")
+    //@Scheduled(cron = "${frequency}")
     public void findByPinCodeAndDateCron() throws IOException, URISyntaxException {
         String localDate = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
         HttpHeaders headers = new HttpHeaders();
@@ -46,6 +53,7 @@ public class CowinService {
         HttpEntity request = new HttpEntity(headers);
         ResponseEntity<CalendarResponseSchemaList> calendarResponseSchemaList = restTemplate.exchange(String.format("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=%s&date=%s", pinCode, localDate), HttpMethod.GET, request, CalendarResponseSchemaList.class);
         Stream<CalendarResponseSchema> calendarResponseSchemaStream;
+        log.info("response");
 
         if (ageLimit.equals("")) {
             calendarResponseSchemaStream = calendarResponseSchemaList.getBody().getCenters().stream().filter(calendarResponseSchema -> calendarResponseSchema.getSessions().stream().filter(session -> session.getAvailable_capacity() > 0).count() > 0);
@@ -54,6 +62,38 @@ public class CowinService {
         }
         List<CalendarResponseSchema> collect = calendarResponseSchemaStream.collect(Collectors.toList());
 
+
+        if (collect.size() > 0) {
+            Runtime runtime = Runtime.getRuntime();
+            log.info("Slot available for booking. Head to www.cowin.gov.in");
+            if (System.getProperty("os.name").contains("Windows")) {
+                runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+                runtime.exec("rundll32 url.dll,FileProtocolHandler " + "https://selfregistration.cowin.gov.in/");
+            } else if (System.getProperty("os.name").contains("Mac")) {
+                runtime.exec("open " + url);
+                runtime.exec("open " + "https://selfregistration.cowin.gov.in/");
+            }
+        }
+    }
+
+    @Scheduled(cron = "${frequency}")
+    public void findByDistrict() throws IOException {
+        String localDate = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("accept", "application/json");
+        headers.set("user-agent", "Mozilla/5.0");
+        HttpEntity request = new HttpEntity(headers);
+        String cowinurl =String.format(cowinBaseUrlSearchByDistrict, districtCode, localDate);
+        log.info("Hitting Cowin with url {}",cowinurl);
+        ResponseEntity<CalendarResponseSchemaList> calendarResponseSchemaList = restTemplate.exchange(cowinurl, HttpMethod.GET, request, CalendarResponseSchemaList.class);
+        Stream<CalendarResponseSchema> calendarResponseSchemaStream;
+        log.info("Hitting Cowin with response {}",calendarResponseSchemaList);
+        if (ageLimit.equals("")) {
+            calendarResponseSchemaStream = calendarResponseSchemaList.getBody().getCenters().stream().filter(calendarResponseSchema -> calendarResponseSchema.getSessions().stream().filter(session -> session.getAvailable_capacity() > 0).count() > 0);
+        } else {
+            calendarResponseSchemaStream = calendarResponseSchemaList.getBody().getCenters().stream().filter(calendarResponseSchema -> calendarResponseSchema.getSessions().stream().filter(session -> session.getAvailable_capacity() > 0 && session.getMin_age_limit() == Integer.parseInt(ageLimit)).count() > 0);
+        }
+        List<CalendarResponseSchema> collect = calendarResponseSchemaStream.collect(Collectors.toList());
 
         if (collect.size() > 0) {
             Runtime runtime = Runtime.getRuntime();
